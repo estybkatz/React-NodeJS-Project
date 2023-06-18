07096b103cda1b2f711f7c48e65e53d3ec310311
Ester Rosenfeld <estybkatz@gmail.com>
2023-06-18 20:05:39 +0300

    origin

--- server - for project/app.js
+++ server - for project/app.js
@@ -10,7 +10,14 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/cards", cardsRouter);
